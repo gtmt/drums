7775aa392cfae9d783a1b3befb58d77e5f4067d8
gtmt <102015986+gtmt@users.noreply.github.com>
2022-03-22 13:47:35 +0530

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,51 @@
+let drums  = document.querySelectorAll(".drum").length;
+for(let i=0;i<drums;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener('click',function () {
+        
+        let button=this.innerHTML;
+        makesound(button);
+
+    
+    });
+
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+});
+function makesound(key){
+    switch(key)
+    {
+        case "a":
+         let tom1= new Audio("sounds/tom-1.mp3");
+        
+          tom1.play();
+      break;
+       case "s":
+           let tom2=new Audio("sounds/tom-2.mp3");
+           tom2.play();
+           break;
+       case "d":
+            let tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+       case "f":
+           let tom4=new Audio("sounds/tom-4.mp3");
+           tom4.play();
+           break;
+       case "j":
+               let crash=new Audio("sounds/crash.mp3");
+               crash.play();
+               break;
+       case "k":
+               let kick=new Audio("sounds/kick-bass.mp3");
+               kick.play();
+               break;
+       case "l":
+                let snare=new Audio("sounds/snare.mp3");
+                 snare.play();
+                break;
+                                                          
+
+    }
+}
